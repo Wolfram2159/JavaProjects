b6902d04536a4186f5cc206a0ccd76196c07d19f
jakub215900 <lol215900>
2019-04-15 14:49:35 +0200

    lab07 progress

--- lab07/src/sample/Controller.java
+++ lab07/src/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -13,6 +17,25 @@ public class Controller {
         System.out.println("second");
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.RED);
-        gc.fillRect(0,0,100,100);
+        //gc.fillRect(0,0,400,400);
+
+        for (double i = -8; i < 8; i+=0.05) {
+            for (double j = -8; j < 8; j+=0.05) {
+                System.out.println(Equation.calc(i,j));
+                if(Equation.calc(i,j)){
+                    gc.setFill(Color.RED);
+                    gc.fillRect(i+200,j+200,1,1);
+                }else{
+                    gc.setFill(Color.BLUE);
+                    gc.fillRect(i+200,j+200,1,1);
+                }
+                /*BufferedImage bi= new BufferedImage(600, 400,
+                        BufferedImage.TYPE_INT_RGB);
+                Color color = new Color(255,0,0,100);
+                bi.setRGB(i, j, );
+                gc.drawImage(SwingFXUtils.toFXImage(bi, null), 0,0
+                );*/
+            }
+        }
     }
 }
