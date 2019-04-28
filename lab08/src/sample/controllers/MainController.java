debe2ec8db739b0d32f7c09ca157bc589156e7e4
jakub215900 <lol215900>
2019-04-28 17:59:50 +0200

    create lab09fx

--- lab08/src/sample/controllers/MainController.java
+++ lab08/src/sample/controllers/MainController.java
@@ -14,7 +14,6 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import javafx.event.ActionEvent;
--- lab09fx/src/sample/entities/Actor.java
+++ lab09fx/src/sample/entities/Actor.java
@@ -1,9 +1,10 @@
-package com.company.entities;
+package sample.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.util.Objects;
 
 @JsonDeserialize(as = Actor.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -14,10 +15,15 @@ public class Actor {
     private String name;
 
     @Override
-    public String toString() {
-        return "Actor{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Actor actor = (Actor) o;
+        return id.equals(actor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
